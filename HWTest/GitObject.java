6cbf03473f8fd453072fb88c46f34965ec95ac6a
Vanilla Chen <2000022772@stu.pku.edu.cn>
2021-01-02 15:45:38 +0800

    修改GitObject类，增加save方法，使其能存储到自身文件中。

--- HWTest/GitObject.java
+++ HWTest/GitObject.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 public abstract class GitObject implements Serializable, Comparable<GitObject>{
     private String key;
+    private boolean isSaved = false;
     //Serializable要的ID
     private static final long serialVersionUID =9876543212345L;
 
@@ -19,5 +20,17 @@ public abstract class GitObject implements Serializable, Comparable<GitObject>{
 
     public int compareTo(GitObject gitObject) {
         return key.compareTo(gitObject.key);
+    }
+
+    //给每个GitObject都添加save方法，直接从object这里存
+    public void save(){
+        if(isSaved) return; //存过了就不用存了
+        ObjectStore.add(this);
+        isSaved = true;
+    }
+
+    //查看存储情况
+    public boolean isSaved(){
+        return isSaved;
     }
 }
